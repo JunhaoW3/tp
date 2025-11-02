a4b9c5af34b143e48ac7438cb90107854357db4b
oyanzhi <ongyanzhi15@gmail.com>
2025-11-02 22:33:40 +0800

    Add method to modelstub

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -211,6 +211,11 @@ public class AddCommandTest {
         }
 
         @Override
+        public void editPersonGeneralReminderList(Person toEdit, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
         public void updateFilteredPersonList(Predicate<Person> predicate) {
             throw new AssertionError("This method should not be called.");
         }
